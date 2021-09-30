7908c2f3b1df22f6cf90363ab3f96fc7c4f61236
Prashant <prashantsingh1549@gmail.com>
2021-09-30 19:51:45 +0530

    Basic Signup Validation

--- app/src/main/java/com/codersoftech/Trademo/SignUpActivity.java
+++ app/src/main/java/com/codersoftech/Trademo/SignUpActivity.java
@@ -5,6 +5,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextUtils;
+import android.text.TextWatcher;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -38,10 +42,45 @@ public class SignUpActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
 
-        name=findViewById(R.id.name);
-        email=findViewById(R.id.email);
-        phone=findViewById(R.id.phone);
-        button=findViewById(R.id.Register);
+        initView();
+
+        name.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                if(!name.getText().toString().matches("^[A-Za-z]+$")){
+                    name.setError("Enter valid name");
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+
+        email.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                if (!isEmail( email )){
+                    email.setError("Enter valid Email Address");
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
 
 
         button.setOnClickListener(new View.OnClickListener() {
@@ -49,19 +88,45 @@ public class SignUpActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
 
-                Name=name.getText().toString();
-                Phone=phone.getText().toString();
-                Email=email.getText().toString();
+                Name=name.getText().toString().trim();
+                Phone=phone.getText().toString().trim();
+                Email=email.getText().toString().trim();
                 Pan="4567";
                 Aadhar="98689";
                 Demat="Dematno1";
                 Wallet="10000";
 
-                register();
-
+                if (Name.isEmpty()){
+                    name.setError("Enter valid name");
+                    name.requestFocus();
+                }else if(!name.getText().toString().matches("^[A-Za-z]+$")){
+                    name.setError("Enter valid name");
+                    name.requestFocus();
+                }else if (Phone.isEmpty()) {
+                    phone.setError("Please enter 10 digit valid Phone Number");
+                    phone.requestFocus();
+                } else if (Phone.length() < 10) {
+                    phone.setError("Please enter 10 digit valid Phone Number");
+                    phone.requestFocus();
+                }else if (Email.isEmpty()) {
+                    email.setError("Enter valid Email Address");
+                    email.requestFocus();
+                }else if (!isEmail( email )){
+                    email.setError("Enter valid Email Address");
+                    email.requestFocus();
+                }else {
+                    register();
+                }
 
             }
     });
+    }
+
+    private void initView() {
+        name=findViewById(R.id.name);
+        email=findViewById(R.id.email);
+        phone=findViewById(R.id.phone);
+        button=findViewById(R.id.Register);
     }
 
     public  String genrate(){
@@ -105,4 +170,15 @@ public class SignUpActivity extends AppCompatActivity {
             }
         });
     }
+
+    boolean isEmail(EditText email)
+    {
+        CharSequence input = email.getText().toString();
+        return(!TextUtils.isEmpty(input)&& Patterns.EMAIL_ADDRESS.matcher(input).matches());
+    }
+
+    private boolean isValidMobile(String phone) {
+        return android.util.Patterns.PHONE.matcher(phone).matches();
+    }
+
 }

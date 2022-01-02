028cbbd2deeadf92dce780187c620b275226a5cc
Aditya Ranjan <ranjanaditya263@gmail.com>
2022-01-02 17:54:15 +0530

    gradle update

--- app/src/main/java/com/codersoftech/Trademo/HomeFragment.java
+++ app/src/main/java/com/codersoftech/Trademo/HomeFragment.java
@@ -15,58 +15,21 @@ import android.widget.Toast;
 
 import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.views.YouTubePlayerView;
 
+import java.sql.Date;
 import java.sql.Time;
+import java.text.SimpleDateFormat;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link HomeFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class HomeFragment extends Fragment {
 
-    Button button,qview,login;
-    SpeechRecognizer speechRecognizer;
-    Time time;
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public HomeFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment HomeFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static HomeFragment newInstance(String param1, String param2) {
-
-        HomeFragment fragment = new HomeFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
+    Button button,qview;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+
     }
 
     @Override
@@ -85,8 +48,15 @@ public class HomeFragment extends Fragment {
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
+                int hours = new Time(System.currentTimeMillis()).getHours();
+
+                /*SimpleDateFormat dateFormat = new SimpleDateFormat("hh:mm:ss a");
+                String formattedDate = dateFormat.format(new Date()).toString();
+                System.out.println(formattedDate);*/
+
                 Toast.makeText(getContext(),"Market is Open",Toast.LENGTH_LONG).show();
-                Log.i("Status","Close");
+                Log.d("YourResponse","Close---"+hours);
             }
         });
         qview.setOnClickListener(new View.OnClickListener() {
